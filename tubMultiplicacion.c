16ce4b4dcad6146d5ff027b8258252f8d9269f17
ErickNatanael <31288885+ErickNatanael@users.noreply.github.com>
2017-11-29 23:58:14 -0600

    Add files via upload

---
+++ tubMultiplicacion.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void main(){
+	int a=12;
+	int b=20;
+	int c;
+	c=a*b;
+	printf("Res: ,%i",c);
+	return;
+}
---
+++ tubSuma.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void main(){
+	int sumando=15;
+	int sumObt;
+	int result;
+	scanf("%i",&sumObt);
+	result=sumando+sumObt;
+	printf("\nEl resultado es: %i \n",result);
